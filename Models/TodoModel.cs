3d8e0db13a2922eb5f697c194ec86f484eaff78a
Alex <licoms1997@gmail.com>
2023-08-07 07:08:40 +0300

    Create Models folder | TodoModels class

---
+++ Models/TodoModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    class TodoModel
+    {
+        /// <summary>
+        /// Current ToDo row creation date
+        /// </summary>
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone; 
+        private string _text;
+
+        public bool isDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        public string Text
+        {
+            get { return Text; }
+            set { Text = value; }
+        }
+
+
+    }
+}
